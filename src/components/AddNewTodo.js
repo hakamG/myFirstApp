d1a075aac3e0cd42b51e272846ca4e9b917d717b
hakamkittany <hakam@guesty.com>
2018-12-23 14:14:57 +0200

    add react router to the app , add new todo as separate component with direct route, enter inside text area will behave like click on add todo button , after adding new todo will redirect to main page (todo list)

---
+++ src/components/AddNewTodo.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { addTodo, updateTextArea } from '../actions/todo.action';
+import connect from 'react-redux/es/connect/connect';
+
+const AddNewTodo = (props) => {
+  const { updateTextArea, textArea, addTodo, history } = props;
+  const onAddTodo = () => {
+    addTodo();
+    history.push('/');
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <div className="add-new">
+            <h1>Add New Todo</h1>
+            <div>
+            <textarea
+              className="form-control"
+              placeholder="New todo"
+              rows="3"
+              onChange={(e) => {updateTextArea(e.target.value);}}
+              onKeyPress={(e) => {
+                if (e.charCode === 13) {
+                  onAddTodo();
+                  e.preventDefault();
+                }
+              }}
+              value={textArea}
+            ></textarea>
+            </div>
+            <div>
+              <Button
+                outline
+                className="add-todo"
+                color="success"
+                onClick={onAddTodo}
+              >Add Todo
+              </Button>
+            </div>
+            <div>
+              <Link to="/">Back to Todo List</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function mapStateToProps(state, props) {
+  return {
+    textArea: state.todo.textArea,
+  };
+}
+
+const mapDispatchToProps = {
+  addTodo,
+  updateTextArea,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddNewTodo);
